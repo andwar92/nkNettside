016b232fce23043778a84a34b0f98099466bf700
Licentiia <taharok@outlook.com>
2025-10-17 11:24:19 +0200

    Refactor assignmentPage structure in model.js to improve data organization

--- utility/model.js
+++ utility/model.js
@@ -7,13 +7,33 @@ const model = {
             username: '',
             password: '',
         },
+        assignmentPage: {
+            assignmentID: 0,
+            title: '',
+            description: '',
+            subject: '',
+            tag: '',
+            groupID: '',
+            date: '',
+            attachment: null,
+        }
     },
     data: {
-        users:
+        users: {
+            userID: 0,
+            username: 'admin',
+            password: 'admin',
+        },
 
-        {
-            username: '',
+        assignmentPage: {
+            assignmentID: 0,
+            title: 'Growth Mindset',
+            description: 'Oppgaveinnhold om Growth Mindset',
+            subject: 'Growth Mindset',
+            tag: 'Fixed vs Growth Mindset',
+            groupID: [0,1],
+            date: '10/17/2025',
+            attachment: 'growth_mindset.pdf',
         }
-
     },
 }
